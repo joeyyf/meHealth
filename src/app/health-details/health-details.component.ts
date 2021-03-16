564c2347bfa6fdebf5efc352a509c85d0cebd683
Joey <snaptraxlive@live.de>
2021-03-16 21:58:02 +0100

    clean up

--- src/app/health-details/health-details.component.ts
+++ src/app/health-details/health-details.component.ts
@@ -33,11 +33,11 @@ export class HealthDetailsComponent implements OnInit {
 
         const heartRates = this.healthModels.map(x => x.heartRate);
         const sumHeartRates = heartRates.reduce((a, b) => a + b, 0);
-        this.heartRateAvg = (sumHeartRates / heartRates.length) || 0;
+        this.heartRateAvg = (sumHeartRates / heartRates.filter(x => x > 0).length) || 0;
 
         const rawIntensities = this.healthModels.map(x => x.rawIntensity);
         const sumRawIntensities = rawIntensities.reduce((a, b) => a + b, 0);
-        this.rawIntensityAvg = (sumRawIntensities / rawIntensities.length) || 0;
+        this.rawIntensityAvg = (sumRawIntensities / rawIntensities.filter(x => x > 0).length) || 0;
 
         const steps = this.healthModels.map(x => x.steps);
         this.stepCountTotal = steps.reduce((a, b) => a + b, 0);
--- src/app/smart-watch.service.ts
+++ src/app/smart-watch.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HealthModel } from './healthModel';
 import { HealthWrapperModel } from './healthWrapperModel';
-import { HEALTHMODELS } from './mock-healthModels';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
@@ -13,13 +12,16 @@ import { catchError, map, tap } from 'rxjs/operators';
 export class SmartWatchService {
 
   tempHealthModels: HealthModel[] = [];
-  private healthDataUrl = 'http://localhost:8000/api/healthData/';  // URL to web api
+  private healthDataUrl = 'http://fileserver:8000/api/healthData/';  // URL to web api
 
   constructor(
     private messageService: MessageService,
     private http: HttpClient
   ) { }
 
+  /**
+  * Get health data from REST service api by given date
+  */
   getHealthData(dateToShow: Date): Observable<HealthWrapperModel> {
 
     //get and format date for api call
@@ -37,33 +39,9 @@ export class SmartWatchService {
     );
   }
 
-  getMockHealthData(): Observable<HealthModel[]> {
-    //filter heart rates ... db has several 0 and 255 !?
-    var filterHeartRate = function (element: HealthModel, index, array) {
-      return (element.heartRate > 20);
-    }
-
-    this.tempHealthModels = HEALTHMODELS.filter(filterHeartRate);
-    for (var v in this.tempHealthModels)
-    {
-      //add human readable date time
-      this.tempHealthModels[v].date = new Date(this.tempHealthModels[v].timestamp * 1000);  // * 1000 for milli seconds
-    }
-
-    //sort by date time
-    var compareDate = function (health1: HealthModel, health2: HealthModel) {
-      var health1Date = new Date(health1.date).getTime();
-      var health2Date = new Date(health2.date).getTime();
-      return health1Date > health2Date ? 1 : -1;
-    }
-
-    //as observable and sorted
-    const healthModels = of(this.tempHealthModels.sort(compareDate));
-    this.messageService.add('smartWatchService: fetched health data');
-    return healthModels;
-  }
-
-  /** Log a SmartWatchService message with the MessageService */
+  /**
+  * Log a SmartWatchService message with the MessageService
+  */
   private log(message: string) {
     this.messageService.add(`SmartWatchService: ${message}`);
   }
